521e5c6b73d610b5f7892c1b851d8fc628083920
parmin-arababadi <parminaman.lov@gmail.com>
2025-08-06 04:58:01 -0700

    fibounachi

--- index.php
+++ index.php
@@ -70,4 +70,17 @@ if ($a==$y) {
     echo 'عدد جز سری فیبوناچی است <br>';
 }
  else  {echo 'عدد جز سری فیبوناچی نیست<br>';}
+
+ $y=2;
+ $x= 0;
+ $n= 1;
+ $a=$x+$n;
+ $count=0;
+ $count=$count+1;
+while ($count==$y) {
+$x=$n;
+$n=$a;
+$a=$x+$n;
+$count++;}
+echo $a .'<br>';
 ?>
---
+++ عدد nام فیبوناچی.php
@@ -0,0 +1,14 @@
+ <?php
+ $y=2;
+ $x= 0;
+ $n= 1;
+ $a=$x+$n;
+ $count=0;
+ $count=$count+1;
+while ($count==$y) {
+$x=$n;
+$n=$a;
+$a=$x+$n;
+$count++;}
+echo $a .'<br>';
+?>
