08a237adc6f352fa14041692de6771176c90bd30
parmin-arababadi <parminaman.lov@gmail.com>
2025-08-12 10:59:25 -0700

    ezafe kardan form

--- ex1.php
+++ ex1.php
@@ -1,5 +1,25 @@
+<html>
+
+<head>
+    <title>square</title>
+</head>
+
+<body>
+    <div style="text-align:right; justify-content:right; position:rtl">
+        <pre>اندازه ی ضلع مربع مورد نظر را بنویسید</pre>
+        <form method="get">
+            <input type="text" name="square" id="square" style="margin:10px;">
+            <label for="square"></label><br>
+            <input type="submit" name="submit" id="submit" value="send." style="margin:10px;">
+            <label for="submit"></label>
+        </form>
+    </div>
+</body>
+
+</html>
 <?php
-$side = 7;
+$_GET["square"];
+$side = $_GET["square"];
 $p = $side * 4;
 $s = $side * $side;
 echo $s . '**' . $p . '<br>';
--- ex3.php
+++ ex3.php
@@ -1,5 +1,18 @@
+<html>
+    <head></head>
+    <body><div style="text-align: right; justify-content: right; position: rtl;">
+        <pre>کیلومتر مورد نظرتان را وارد کنید</pre> 
+        <form method="post">
+            <input type="text" id="km" name="km" style="margin: 10px;">
+            <label for="km"></label><br>
+            <input type="submit" id="submit" name="submit" value="send" style="margin: 10px;">
+            <label for="submit"></label>
+        </form>
+        </div>
+    </body>
+</html>
 <?php
-$km = 10;
+$km = $_POST["km"];
 $m = $km * 1000;
-echo '***' . $m . '<br>';
+echo $m . '<br>';
 ?>
