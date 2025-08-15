b6383e9342985e98b8eb6c380f8f84ba9cff0caa
parmin-arababadi <parminaman.lov@gmail.com>
2025-08-15 09:48:08 -0700

    ezafe kardan form

--- age.php
+++ age.php
@@ -1,5 +1,22 @@
+<html>
+
+<head></head>
+
+<body>
+<body>
+    <div style="text-align:right; justify-content:right; position:rtl">
+        <form method="post">
+                        <label for="age">سال تولد خود را وارد کنید</label>
+            <input type="text" name="age" id="age" style="margin:10px;"><br>
+            <input type="submit" name="submit" id="submit" value="send." style="margin:10px;">
+            <label for="submit"></label>
+        </form>
+    </div>
+</body>
+
+</html>
 <?php 
-$birth = 1380;
+$birth = $_POST["age"];
 $age = 1404 - $birth;
 if ($age > 18) {
     echo 'شما میتوانید رای دهید';
